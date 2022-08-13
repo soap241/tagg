34536955c64bc2aca9db5f80a86dccca4a673ee9
Moro-Afriyie <ceremoinc99@gmail.com>
2022-08-13 16:25:32 +0000

    update the payments

--- ui/src/app/views/dashboard/components/payment-card/payment-card.component.ts
+++ ui/src/app/views/dashboard/components/payment-card/payment-card.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { IProject } from "../../../../models/@types";
+import { DataService } from "../../../../services/data/data.service";
 
 @Component({
-  selector: 'app-payment-card',
-  templateUrl: './payment-card.component.html',
-  styleUrls: ['./payment-card.component.css']
+  selector: "app-payment-card",
+  templateUrl: "./payment-card.component.html",
+  styleUrls: ["./payment-card.component.css"],
 })
 export class PaymentCardComponent implements OnInit {
+  @Input() project: IProject = {} as IProject;
+  id: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.id = params.id;
+    });
   }
-
 }
--- ui/src/app/views/dashboard/payments/payments.component.ts
+++ ui/src/app/views/dashboard/payments/payments.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IProject } from "../../../models/@types";
+import { DataService } from "../../../services/data/data.service";
 
 @Component({
-  selector: 'app-payments',
-  templateUrl: './payments.component.html',
-  styleUrls: ['./payments.component.css']
+  selector: "app-payments",
+  templateUrl: "./payments.component.html",
+  styleUrls: ["./payments.component.css"],
 })
 export class PaymentsComponent implements OnInit {
+  projects: IProject[] = [];
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.projects = this.dataService.getProjects();
   }
-
 }
